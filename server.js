480aad29d5c52b23e13e4df03ffc81b1c70425f7
Sara <sara@Saras-MacBook-Pro.local>
2020-05-01 20:31:47 +0200

    setting up api for adding and removing user

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import data from './data.json'
 
+let userList = data
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -16,6 +18,41 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/users', (req, res) => {
+  res.json(userList)
+})
+// add new user
+app.post('/users', (req, res) => {
+  const user = req.body;
+  console.log(user)
+  userList.push(user)
+  res.send('new user added')
+})
+// delete end-point
+app.delete('/users/id/:id',(req, res) =>{
+  const user = req.params.id
+      userList = userList.filter(item => {
+        if (item.id !== user) {
+            return true;
+        }
+        return false;
+    });
+
+    res.send('User is deleted');
+})
+
+app.delete('/users/id/:id',(req, res) =>{
+  const user = req.params.id
+      userList = userList.filter(item => {
+        if (item.id !== user) {
+            return true;
+        }
+        return false;
+    });
+
+    res.send('User is deleted');
 })
 
 // Start the server
